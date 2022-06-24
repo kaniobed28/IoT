8ec34a84f7d87fe455d3d8bc7f56dd9becdb84c8
Kani Obed <kaniobed28@gmail.com>
2022-06-24 22:21:08 +0000

    soil.py comment added

--- CounterFit/soil.py
+++ CounterFit/soil.py
@@ -1,9 +1,15 @@
+
+"""
+CounterFit is used to  virtually connect sensors.
+In this code I used the virtual soil moisture sensor.
+"""
 import time
 from counterfit_connection import CounterFitConnection
-from counterfit_shims_grove.adc import ADC
-CounterFitConnection.init('127.0.0.1',5000)
+from counterfit_shims_grove.adc import ADC #Importing ANALOGUE DIGITAL CONVERTOR
+CounterFitConnection.init('127.0.0.1',5000) # Initializing the counterfit connction.
+# The sensors can be accessed through the browser on local host port 5000.
 adc = ADC()
 while True:
     time.sleep(2)
-    sensor = adc.read(1)
-    print("The soil moisture of the soil is",sensor)
+    sensor = adc.read(1) # Virtually reading the soil moisture. 
+    print("The soil moisture of the soil is",sensor) # Printing the sensor reading on screen.
