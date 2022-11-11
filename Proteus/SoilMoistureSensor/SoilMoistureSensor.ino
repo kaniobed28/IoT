df9ec8b90a9e7a58847a87fd18b6830caa1106b6
Kani Obed <81527514+kaniobed28@users.noreply.github.com>
2022-11-11 17:24:36 +0000

    Add files via upload

---
+++ Proteus/SoilMoistureSensor/SoilMoistureSensor.ino
@@ -0,0 +1,25 @@
+#define sensorpin A0
+float result;
+void setup(){
+  pinMode(sensorpin,INPUT);
+  Serial.begin(9600);
+}
+void loop(){
+  result = analogRead(sensorpin);
+  delay(100);
+  Serial.println("soil moisture");
+//  Serial.println(result);
+  if (result >=800){
+  Serial.println("very dry soil");
+  }
+  else if (result >=500){
+  Serial.println("dry soil");
+  }
+  else if (result >=250){
+  Serial.println("wet soil");
+  }
+  else{
+  Serial.println("very wet soil");
+  }
+
+}
